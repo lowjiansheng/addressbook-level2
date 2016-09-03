3bab133fcbaa8b72a8d3eff4e5010331076c5198
ljsjack <lowjiansheng@hotmail.sg>
2016-09-03 22:01:29 +0800

    Add NameTest class. Add testIsSimilar method to check for similar names.

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,22 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class NameTest {
+
+
+	@Test
+	public void testIsSimilar() throws IllegalValueException {
+		String testName1 = "K Smith";
+		String testName2 = "Smith K";
+		Name name1 = new Name(testName1);
+		Name name2 = new Name(testName2);
+		
+		assertTrue(isSimilar(name1, name2);
+	}
+
+}
